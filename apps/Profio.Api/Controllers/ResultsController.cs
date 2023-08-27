797bb84ade840acf739475807677c87da6a75e54
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2023-08-27 18:44:26 +0700

    add reponse cache

--- apps/Profio.Api/Controllers/ResultsController.cs
+++ apps/Profio.Api/Controllers/ResultsController.cs
@@ -6,27 +6,42 @@ namespace Profio.Api.Controllers;
 public class ResultsController : ControllerBase
 {
   [HttpGet("not-found")]
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
   public IActionResult GetNotFound()
         => NotFound();
+
   [HttpGet("ok")]
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
   public IActionResult GetOk()
       => Ok();
+
   [HttpGet("bad-request")]
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
   public IActionResult GetBadRequest()
       => BadRequest();
+
   [HttpGet("no-content")]
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
   public IActionResult GetNoContent()
       => NoContent();
+
   [HttpGet("unauthorized")]
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
   public IActionResult GetUnauthorized()
       => Unauthorized();
+
   [HttpGet("conflict")]
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
   public IActionResult GetConflict()
       => Conflict();
+
   [HttpGet("forbid")]
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
   public IActionResult GetForbid()
       => Forbid();
+
   [HttpGet("internal-server-error")]
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
   public IActionResult GetInternalServerError()
       => StatusCode(StatusCodes.Status500InternalServerError);
 }
