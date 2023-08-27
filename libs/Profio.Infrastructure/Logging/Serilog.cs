31666481c134cd12b14b244fe0ecab8b23ed7d3a
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2023-08-27 12:03:48 +0700

    add 3rd service

--- libs/Profio.Infrastructure/Logging/Serilog.cs
+++ libs/Profio.Infrastructure/Logging/Serilog.cs
@@ -5,4 +5,5 @@ public class Serilog
   public bool UseConsole { get; set; } = true;
   public string LogTemplate { get; set; } =
     "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} {Level} - {Message:lj}{NewLine}{Exception}";
+  public string? SeqUrl { get; set; } = "http://localhost:5341";
 }
