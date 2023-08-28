4fcb1e4e33f00879c2e6f7b1d8152ad50704b960
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-08-28 16:03:37 +0700

    relocate the Migrations folder and Optimize DbContext

--- libs/Profio.Infrastructure/Persistence/Relational/Migrations/ApplicationDbContextModelSnapshot.cs
+++ libs/Profio.Infrastructure/Persistence/Relational/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -2,21 +2,18 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Profio.Infrastructure.Persistence.Relational;
 
 #nullable disable
 
-namespace Profio.Infrastructure.Migrations
+namespace Profio.Infrastructure.Persistence.Relational.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20230828053053_AddFullNameForUser")]
-    partial class AddFullNameForUser
+    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
     {
-        /// <inheritdoc />
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
