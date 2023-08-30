3233e9cd705338eb806c856b7499c605899734ce
Nguyễn Xuân Nhân <nguyenxuannhan407@gmail.com>
2023-08-30 21:13:21 +0700

    Delete Database/Scripts directory

--- Database/Scripts/__EFMigrationsHistory.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion") VALUES ('20230828085054_Init', '7.0.10');
-INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion") VALUES ('20230830094309_InitBussinessEntities', '7.0.10');
