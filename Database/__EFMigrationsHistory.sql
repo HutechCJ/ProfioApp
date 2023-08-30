e684d5d373f65eaa5b872a2397c770361c4092ed
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2023-08-30 21:12:05 +0700

    add backup db

---
+++ Database/__EFMigrationsHistory.sql
@@ -0,0 +1,2 @@
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion") VALUES ('20230828085054_Init', '7.0.10');
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion") VALUES ('20230830094309_InitBussinessEntities', '7.0.10');
