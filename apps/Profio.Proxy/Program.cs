e904b211c6d14842844d3cc5982a24528769957a
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2023-09-01 00:21:38 +0700

    add proxy

---
+++ apps/Profio.Proxy/Program.cs
@@ -0,0 +1,17 @@
+using Profio.Infrastructure.OpenTelemetry;
+using Spectre.Console;
+
+var builder = WebApplication.CreateBuilder(args);
+
+AnsiConsole.Write(new FigletText("Profio Proxy").Centered().Color(Color.BlueViolet));
+
+builder.Services.AddReverseProxy()
+  .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.AddOpenTelemetry();
+
+var app = builder.Build();
+
+app.MapReverseProxy();
+app.MapHealthChecks("/health");
+app.Run();
