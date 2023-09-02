478fa84286c4fd3cf74a9018520733474d83b8a8
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-09-02 20:11:53 +0700

    update post method only return id

--- apps/Profio.Api/Controllers/v1/CustomersController.cs
+++ apps/Profio.Api/Controllers/v1/CustomersController.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Profio.Application.Customers.Commands;
-using Profio.Application.Customers.Queries;
 
 namespace Profio.Api.Controllers.v1;
 
@@ -13,12 +12,11 @@ public class CustomersController : BaseController
   public async Task<IActionResult> Post(CreateCustomerCommand command)
   {
     var result = await Mediator.Send(command);
-    var dto = await Mediator.Send(new GetCustomerByIdQuery(result.Data));
 
     var domain = HttpContext.Request.GetDisplayUrl();
-    var routeTemplate = ControllerContext.ActionDescriptor.AttributeRouteInfo.Template;
-    var apiVersion = HttpContext.GetRequestedApiVersion().ToString();
+    var routeTemplate = ControllerContext.ActionDescriptor.AttributeRouteInfo!.Template;
+    var apiVersion = HttpContext.GetRequestedApiVersion()!.ToString();
 
-    return Created($"{domain}/{routeTemplate.Replace("{version:apiVersion}", apiVersion)}/{result.Data}", dto);
+    return Created($"{domain}/{routeTemplate!.Replace("{version:apiVersion}", apiVersion)}/{result.Data}", result);
   }
 }
