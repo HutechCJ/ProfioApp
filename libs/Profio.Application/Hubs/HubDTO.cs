f5ef90d12a207799390610194f6db0c21cf530e0
NhonVo <vothuongtruongnhon2002@gmail.com>
2023-09-05 14:02:19 +0700

    remove unuse list in hubdto

--- libs/Profio.Application/Hubs/HubDTO.cs
+++ libs/Profio.Application/Hubs/HubDTO.cs
@@ -11,7 +11,4 @@ public record HubDto : BaseModel
   public required string? ZipCode { get; set; }
   public Location? Location { get; set; }
   public HubStatus Status { get; set; } = HubStatus.Active;
-  public ICollection<OrderHistory>? OrderHistories { get; set; } = new List<OrderHistory>();
-  public ICollection<Route>? StartRoutes { get; set; } = new List<Route>();
-  public ICollection<Route>? EndRoutes { get; set; } = new List<Route>();
 }
