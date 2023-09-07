88cd58121a0664ae8113f9a266cfe61c6258f800
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-09-07 16:23:48 +0700

    add delivery progresses endpoints

---
+++ libs/Profio.Application/DeliveryProgresses/Commands/CreateDeliveryProgressCommand.cs
@@ -0,0 +1,46 @@
+using AutoMapper;
+using EntityFrameworkCore.UnitOfWork.Interfaces;
+using FluentValidation;
+using Profio.Application.CQRS.Events.Commands;
+using Profio.Application.CQRS.Handlers.Command;
+using Profio.Application.CQRS.Validators;
+using Profio.Application.Orders.Validators;
+using Profio.Domain.Entities;
+using Profio.Domain.ValueObjects;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Profio.Application.DeliveryProgresses.Commands;
+
+[SwaggerSchema(
+  Title = "Delivery Progress Create Request",
+  Description = "A Representation of list of Delivery Progress")]
+public record CreateDeliveryProgressCommand : CreateCommandBase
+{
+  public Location? CurrentLocation { get; set; }
+  public byte PercentComplete { get; set; } = 0;
+  //public TimeSpan? EstimatedTimeRemaining { get; set; }
+  public DateTime? LastUpdated { get; set; }
+  public required string OrderId { get; set; }
+
+}
+
+public class CreateDeliveryProgressCommandHandler : CreateCommandHandlerBase<CreateDeliveryProgressCommand, DeliveryProgress>
+{
+  public CreateDeliveryProgressCommandHandler(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper)
+  {
+  }
+}
+
+public class CreateDeliveryProgressCommandValidator : AbstractValidator<CreateDeliveryProgressCommand>
+{
+  public CreateDeliveryProgressCommandValidator(OrderExistenceByIdValidator orderValidator)
+  {
+    RuleFor(c => c.CurrentLocation)
+      .SetValidator(new LocationValidator()!);
+
+    RuleFor(c => c.PercentComplete);
+
+    RuleFor(c => c.OrderId)
+      .SetValidator(orderValidator);
+  }
+}
---
+++ libs/Profio.Application/DeliveryProgresses/Commands/UpdateDeliveryProgressCommand.cs
@@ -0,0 +1,44 @@
+using AutoMapper;
+using EntityFrameworkCore.UnitOfWork.Interfaces;
+using Profio.Application.CQRS.Events.Commands;
+using Profio.Application.CQRS.Handlers.Command;
+using Profio.Application.CQRS.Validators;
+using Profio.Application.Orders.Validators;
+using Profio.Domain.Entities;
+using Profio.Domain.ValueObjects;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Profio.Application.DeliveryProgresses.Commands;
+
+[SwaggerSchema(
+  Title = "Delivery Progress Update Request",
+  Description = "A Representation of list of Delivery Progress")]
+public record UpdateDeliveryProgressCommand(object Id) : UpdateCommandBase(Id)
+{
+  public Location? CurrentLocation { get; set; }
+  public byte PercentComplete { get; set; } = 0;
+  //public TimeSpan? EstimatedTimeRemaining { get; set; }
+  public DateTime? LastUpdated { get; set; }
+  public string? OrderId { get; set; }
+}
+
+public class UpdateDeliveryProgressCommandHandler : UpdateCommandHandlerBase<UpdateDeliveryProgressCommand, DeliveryProgress>
+{
+  public UpdateDeliveryProgressCommandHandler(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper)
+  {
+  }
+}
+
+public class UpdateDeliveryProgressCommandValidator : UpdateCommandValidatorBase<UpdateDeliveryProgressCommand>
+{
+  public UpdateDeliveryProgressCommandValidator(OrderExistenceByIdValidator orderValidator)
+  {
+    RuleFor(c => c.CurrentLocation)
+      .SetValidator(new LocationValidator()!);
+
+    RuleFor(c => c.PercentComplete);
+
+    RuleFor(c => c.OrderId)
+      .SetValidator(orderValidator!);
+  }
+}
