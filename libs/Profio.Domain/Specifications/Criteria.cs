516fb9bc87a76d72244bd7c20414a15c03a5525d
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-09-07 23:49:02 +0700

    update criteria

--- libs/Profio.Domain/Specifications/Criteria.cs
+++ libs/Profio.Domain/Specifications/Criteria.cs
@@ -8,8 +8,6 @@ public class Criteria<T> where T : IEntity<object>
   public string? OrderBy { get; set; }
   public string? OrderByDescending { get; set; }
   public List<string> IncludeStrings { get; } = new List<string>();
-  public int Skip { get; set; } = 0;
-  public int Take { get; set; } = 0;
   public int PageNumber { get; set; } = 1;
   public int PageSize { get; set; } = 10;
 }
