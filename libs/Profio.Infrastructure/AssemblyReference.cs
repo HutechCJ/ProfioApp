da90f366c5f593398976c6eb35f5037f5035329d
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2023-09-08 18:59:46 +0700

    add migrations

--- libs/Profio.Infrastructure/AssemblyReference.cs
+++ libs/Profio.Infrastructure/AssemblyReference.cs
@@ -7,4 +7,5 @@ public static class AssemblyReference
 {
   public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
   public static readonly string? AssemblyName = typeof(ApplicationDbContext).Assembly.FullName;
+  public static readonly Assembly CallingAssembly = Assembly.GetCallingAssembly();
 }
