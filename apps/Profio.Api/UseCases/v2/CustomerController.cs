7e974aa94238d497632d109ede2483ca0bbe3f3c
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2023-09-09 17:12:13 +0700

    fix api versioning for hub

---
+++ apps/Profio.Api/UseCases/v2/CustomerController.cs
@@ -0,0 +1,18 @@
+using EntityFrameworkCore.Repository.Collections;
+using Microsoft.AspNetCore.Mvc;
+using Profio.Application.Customers.Queries;
+using Profio.Application.Customers;
+using Profio.Domain.Entities;
+using Profio.Domain.Models;
+using Profio.Domain.Specifications;
+
+namespace Profio.Api.UseCases.v2;
+
+[ApiVersion("2.0")]
+public class CustomerController : BaseEntityController<Customer, CustomerDto, GetCustomerByIdQuery>
+{
+  [HttpGet]
+  [MapToApiVersion("2.0")]
+  public Task<ActionResult<ResultModel<IPagedList<CustomerDto>>>> Get([FromQuery] Criteria<Customer> criteria)
+    => HandlePaginationQuery(new GetCustomerWithPagingQuery(criteria));
+}
--- libs/Profio.Application/Abstractions/Idempotency/IdempotentCommand.cs
+++ libs/Profio.Application/Abstractions/Idempotency/IdempotentCommand.cs
@@ -1,5 +1,6 @@
 using MediatR;
+using Profio.Domain.Interfaces;
 
 namespace Profio.Application.Abstractions.Idempotency;
 
-public abstract record IdempotentCommand(Guid RequestId) : IRequest;
+public abstract record IdempotentCommand(Guid RequestId) : IRequest, ITxRequest;
