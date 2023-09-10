f3a21c1d5e9c291960d9f4ae3bc19adcc3fd7b2b
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-09-11 01:10:21 +0700

    add OrderEnumFilter and not change enum to string any more

--- libs/Profio.Application/Orders/Queries/GetOrderWithPagingQuery.cs
+++ libs/Profio.Application/Orders/Queries/GetOrderWithPagingQuery.cs
@@ -5,15 +5,20 @@ using Profio.Application.Abstractions.CQRS.Handlers.Queries;
 using Profio.Application.Abstractions.CQRS.Validators;
 using Profio.Domain.Entities;
 using Profio.Domain.Specifications;
+using System.Linq.Expressions;
 
 namespace Profio.Application.Orders.Queries;
 
-public record GetOrderWithPagingQuery(Criteria Criteria) : GetWithPagingQueryBase<OrderDto>(Criteria);
+public record GetOrderWithPagingQuery(Criteria Criteria, OrderEnumFilter OrderEnumFilter) : GetWithPagingQueryBase<OrderDto>(Criteria);
 
 public class GetOrderWithPagingQueryHandler : GetWithPagingQueryHandler<GetOrderWithPagingQuery, OrderDto, Order>
 {
   public GetOrderWithPagingQueryHandler(IRepositoryFactory unitOfWork, IMapper mapper) : base(unitOfWork, mapper)
   {
+  }
+  protected override Expression<Func<Order, bool>> RequestFilter(GetOrderWithPagingQuery request)
+  {
+    return x => x.Status == request.OrderEnumFilter.Status;
   }
 }
 
