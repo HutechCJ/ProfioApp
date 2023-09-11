3af49da5413ddc51f4cbd39fadc43f0a1e864c63
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-09-11 12:12:32 +0700

    optimize the get entity count

--- libs/Profio.Application/Counters/Queries/GetEntityCountQuery.cs
+++ libs/Profio.Application/Counters/Queries/GetEntityCountQuery.cs
@@ -2,47 +2,43 @@ using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Profio.Domain.Entities;
 using Profio.Infrastructure.Persistence;
-using System.Collections.Immutable;
 
-namespace Profio.Application.Counters.Queries;
-
-public record GetEntityCountQuery(IList<string> EntityTypes) : IRequest<Dictionary<string, int>>;
-public class GetEntityCountQueryHandler : IRequestHandler<GetEntityCountQuery, Dictionary<string, int>>
+namespace Profio.Application.Counters.Queries
 {
-  private readonly ApplicationDbContext _applicationDbContext;
+  public record GetEntityCountQuery(IList<string> EntityTypes) : IRequest<Dictionary<string, int>>;
 
-  public GetEntityCountQueryHandler(ApplicationDbContext applicationDbContext)
-    => _applicationDbContext = applicationDbContext;
-  public Task<Dictionary<string, int>> Handle(GetEntityCountQuery request, CancellationToken cancellationToken)
+  public class GetEntityCountQueryHandler : IRequestHandler<GetEntityCountQuery, Dictionary<string, int>>
   {
-    var entityTypes = new List<Type>
+    private readonly ApplicationDbContext _applicationDbContext;
+    private readonly Dictionary<string, IQueryable<object>> _entitySets;
+
+    public GetEntityCountQueryHandler(ApplicationDbContext applicationDbContext)
     {
-      typeof(Customer),
-      typeof(Delivery),
-      typeof(Incident),
-      typeof(Order),
-      typeof(Staff),
-      typeof(Vehicle)
-    };
+      _applicationDbContext = applicationDbContext;
 
-    var selectedEntityTypes = entityTypes.Where(et => request.EntityTypes.Select(x => x.ToLower()).Contains(et.Name.ToLower()));
+      _entitySets = new Dictionary<string, IQueryable<object>>
+      {
+        [nameof(Customer)] = _applicationDbContext.Set<Customer>(),
+        [nameof(Delivery)] = _applicationDbContext.Set<Delivery>(),
+        [nameof(Incident)] = _applicationDbContext.Set<Incident>(),
+        [nameof(Order)] = _applicationDbContext.Set<Order>(),
+        [nameof(Staff)] = _applicationDbContext.Set<Staff>(),
+        [nameof(Vehicle)] = _applicationDbContext.Set<Vehicle>()
+      };
+    }
 
-    var dictionary = selectedEntityTypes.ToDictionary(x => x.Name, x => GetCount(x));
-
-    return Task.FromResult(dictionary);
-  }
-
-  private int GetCount(Type type)
-  {
-    return type.Name switch
+    public async Task<Dictionary<string, int>> Handle(GetEntityCountQuery request, CancellationToken cancellationToken)
     {
-      nameof(Customer) => _applicationDbContext.Set<Customer>().Count(),
-      nameof(Delivery) => _applicationDbContext.Set<Delivery>().Count(),
-      nameof(Incident) => _applicationDbContext.Set<Incident>().Count(),
-      nameof(Order) => _applicationDbContext.Set<Order>().Count(),
-      nameof(Staff) => _applicationDbContext.Set<Staff>().Count(),
-      nameof(Vehicle) => _applicationDbContext.Set<Vehicle>().Count(),
-      _ => 0
-    };
+      var selectedEntityTypes = _entitySets
+          .Where(pair => request.EntityTypes.Contains(pair.Key, StringComparer.OrdinalIgnoreCase))
+          .ToDictionary(pair => pair.Key, pair => pair.Value);
+
+      var result = new Dictionary<string, int>();
+
+      foreach (var pair in selectedEntityTypes)
+        result[pair.Key] = await pair.Value.CountAsync(cancellationToken: cancellationToken);
+
+      return result;
+    }
   }
 }
