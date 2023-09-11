f22f55d77580627644b17f665031b6902c116ac9
Dat Hoang <dat09t@gmail.com>
2023-09-11 15:41:59 +0700

    get rid of navigation event use nextjs ui streaming and loading

--- apps/cms/app/layout.tsx
+++ apps/cms/app/layout.tsx
@@ -20,9 +20,6 @@ export default function RootLayout({
         <Providers>
           <ThemeRegistry>
             {children}
-            <Suspense fallback={null}>
-              <NavigationEvents />
-            </Suspense>
           </ThemeRegistry>
         </Providers>
       </body>
