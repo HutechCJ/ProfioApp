6f3bec35c768a4235c8b02c63ad58b68ba61d369
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-09-17 16:36:48 +0700

    add description for swagger param entityTypes in CountersController

--- apps/Profio.Api/UseCases/v1/CountersController.cs
+++ apps/Profio.Api/UseCases/v1/CountersController.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Profio.Application.Counters.Queries;
+using Profio.Domain.Entities;
 using Profio.Domain.Models;
 using Swashbuckle.AspNetCore.Annotations;
+using Route = Profio.Domain.Entities.Route;
 
 namespace Profio.Api.UseCases.v1;
 
@@ -12,6 +14,9 @@ public class CountersController : BaseController
   [HttpGet("entities")]
   [SwaggerOperation(summary: "Retrieve the count of items in each table of the entity")]
 
-  public async Task<ActionResult<ResultModel<Dictionary<string, int>>>> GetEntityCount([FromQuery] IList<string> entityTypes)
+  public async Task<ActionResult<ResultModel<Dictionary<string, int>>>> GetEntityCount(
+    [FromQuery]
+    [SwaggerParameter(description: $"{nameof(Customer)}, {nameof(Delivery)}, {nameof(DeliveryProgress)}, {nameof(Incident)}, {nameof(Order)}, {nameof(OrderHistory)}, {nameof(Route)}, {nameof(Staff)}, {nameof(Vehicle)}")]
+      IList<string> entityTypes)
     => Ok(ResultModel<Dictionary<string, int>>.Create(await Mediator.Send(new GetEntityCountQuery(entityTypes))));
 }
