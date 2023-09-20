c957e0bc27707846db542e8d8285c4a8042e9c71
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2023-09-21 00:32:26 +0700

    add retry email sender

--- libs/Profio.Infrastructure/Email/FluentEmail/Internal/EmailService.cs
+++ libs/Profio.Infrastructure/Email/FluentEmail/Internal/EmailService.cs
@@ -1,19 +1,31 @@
 using FluentEmail.Core;
+using Microsoft.Extensions.Logging;
+using Polly;
 
 namespace Profio.Infrastructure.Email.FluentEmail.Internal;
 
 public sealed class EmailService : IEmailService
 {
   private readonly IFluentEmailFactory _fluentEmailFactory;
+  private readonly ILogger<EmailService> _logger;
 
-  public EmailService(IFluentEmailFactory fluentEmailFactory)
-    => _fluentEmailFactory = fluentEmailFactory;
+  public EmailService(IFluentEmailFactory fluentEmailFactory, ILogger<EmailService> logger)
+    => (_fluentEmailFactory, _logger) = (fluentEmailFactory, logger);
 
   public async Task SendEmailAsync(EmailMetadata metadata)
-    => await _fluentEmailFactory
-      .Create()
-      .To(metadata.To)
-      .Subject(metadata.Subject)
-      .UsingTemplateFromFile(metadata.Template, metadata.Model)
-      .SendAsync();
+    => await Policy
+      .Handle<Exception>()
+      .WaitAndRetryAsync(
+        retryCount: 3, 
+        sleepDurationProvider: _ => TimeSpan.FromMilliseconds(new Random().Next(1000, 3000)),
+        onRetry: (_, retryCount, _) =>
+          _logger.LogWarning($"Failed to send email. Retrying... Attempt: {retryCount}")
+      ).ExecuteAsync(async () =>
+        await _fluentEmailFactory
+          .Create()
+          .To(metadata.To)
+          .Subject(metadata.Subject)
+          .UsingTemplateFromFile(metadata.Template, metadata.Model)
+          .SendAsync()
+      );
 }
