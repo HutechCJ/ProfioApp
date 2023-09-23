aeb958b890dbe2322e16b8c58bff997a2c02f605
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2023-09-24 01:51:01 +0700

    add retry sms

--- libs/Profio.Infrastructure/Message/TwilioClient.cs
+++ libs/Profio.Infrastructure/Message/TwilioClient.cs
@@ -1,4 +1,6 @@
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Polly;
 using Twilio.Clients;
 using Twilio.Http;
 using HttpClient = System.Net.Http.HttpClient;
@@ -8,8 +10,9 @@ namespace Profio.Infrastructure.Message;
 public sealed class TwilioClient : ITwilioRestClient
 {
   private readonly ITwilioRestClient _innerClient;
+  private readonly ILogger<TwilioClient> _logger;
 
-  public TwilioClient(IConfiguration config, HttpClient httpClient)
+  public TwilioClient(IConfiguration config, HttpClient httpClient, ILogger<TwilioClient> logger)
   {
     httpClient.DefaultRequestHeaders.Add("X-Custom-Header", "TwilioRestClient");
 
@@ -17,10 +20,25 @@ public sealed class TwilioClient : ITwilioRestClient
       config["Twilio:AccountSid"],
       config["Twilio:AuthToken"],
       httpClient: new SystemNetHttpClient(httpClient));
+
+    _logger = logger;
   }
 
   public Response Request(Request request) => _innerClient.Request(request);
-  public async Task<Response> RequestAsync(Request request) => await _innerClient.RequestAsync(request);
+
+  public async Task<Response> RequestAsync(Request request)
+    => await Policy
+      .Handle<Exception>()
+      .WaitAndRetryAsync(
+        retryCount: 3,
+        sleepDurationProvider: _ => TimeSpan.FromMilliseconds(new Random().Next(1000, 3000)),
+        onRetry: (_, retryCount, _) =>
+          _logger.LogWarning("Failed to send SMS. Retrying... Attempt: {retryCount}", retryCount)
+      ).ExecuteAsync(async () =>
+        await _innerClient
+          .RequestAsync(request)
+      );
+
   public string AccountSid => _innerClient.AccountSid;
   public string Region => _innerClient.Region;
   public Twilio.Http.HttpClient HttpClient => _innerClient.HttpClient;
