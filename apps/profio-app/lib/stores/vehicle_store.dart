14970cb97d75e2ece95b4455e370251a902e1c5c
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-09-25 11:14:46 +0700

    add order and update simulate function

--- apps/profio-app/lib/stores/vehicle_store.dart
+++ apps/profio-app/lib/stores/vehicle_store.dart
@@ -4,6 +4,7 @@ import 'package:profio_staff_client/api/base_api.dart';
 import 'package:profio_staff_client/api/paging.dart';
 import 'package:profio_staff_client/api/result_model.dart';
 import 'package:profio_staff_client/constants/profio.dart';
+import 'package:profio_staff_client/models/order.dart';
 import 'package:profio_staff_client/models/vehicle.dart';
 import 'package:profio_staff_client/stores/hub_store.dart';
 import 'package:provider/provider.dart';
@@ -48,6 +49,18 @@ abstract class VehicleStoreBase with Store {
     await _baseAPI.fetchData(
         '${Profio.baseUrl}/v1/${Profio.vehicleEndpoints}/$vehicleId/${Profio.hubEndpoints}/$hubId/visit',
         method: ApiMethod.POST);
+  }
+
+  Future<List<Order>> fetchOrders() async {
+    if (hasSelectedVehicle) {
+      var data = await _baseAPI
+          .fetchData('https://profio-sv1.azurewebsites.net/api/v1/orders');
+      var result = ResultModel.fromJson(data.object);
+      var paging = Paging.fromJson(result.data);
+      var orders = paging.items.map((item) => Order.fromJson(item)).toList();
+      return orders;
+    }
+    return [];
   }
 
   void onInit(BuildContext context) {
