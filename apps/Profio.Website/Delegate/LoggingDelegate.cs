da1a76d13a92bfa301124688bb6b9438812a6f44
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2023-09-25 14:49:11 +0700

    add delegate for website

---
+++ apps/Profio.Website/Delegate/LoggingDelegate.cs
@@ -0,0 +1,27 @@
+namespace Profio.Website.Delegate;
+
+public class LoggingDelegate : DelegatingHandler
+{
+  private readonly ILogger<LoggingDelegate> _logger;
+
+  public LoggingDelegate(ILogger<LoggingDelegate> logger)
+    => _logger = logger;
+
+  protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+    CancellationToken cancellationToken)
+  {
+    try
+    {
+      _logger.LogInformation("Request: {0}", request);
+      var response = await base.SendAsync(request, cancellationToken);
+      response.EnsureSuccessStatusCode();
+      _logger.LogInformation("Response: {0}", response);
+      return response;
+    }
+    catch (Exception e)
+    {
+      _logger.LogError(e, "Error: {0}", e.Message);
+      throw;
+    }
+  }
+}
