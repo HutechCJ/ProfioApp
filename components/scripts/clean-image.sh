d517e3060ae375155937d7b29ba53fe8ddfc3af2
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2023-10-03 02:11:29 +0700

    fix shell issue

--- components/scripts/clean-image.sh
+++ components/scripts/clean-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker rmi -f $(docker images -f "dangling=true" -q)
-docker rmi -f $(docker images --format  "{{.Repository}}:{{.Tag}}" | grep "none")
+docker rmi -f "$(docker images -f 'dangling=true' -q)"
+docker rmi -f "$(docker images --format  '{{.Repository}}:{{.Tag}}' | grep 'none')"
